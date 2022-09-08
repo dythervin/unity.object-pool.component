e1c75ac7a89e1cd453d94703a393e03a93b4cf11
Dastan Dzhumabekov <dastan.wm@gmail.com>
2022-09-08 13:24:13 +0600

    Fix

--- Runtime/ComponentPool.cs
+++ Runtime/ComponentPool.cs
@@ -1,16 +1,15 @@
-﻿#if ODIN_INSPECTOR
-using Sirenix.OdinInspector;
-#endif
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Dythervin.Callbacks;
 using Dythervin.Core;
 using Dythervin.Core.Extensions;
 using Dythervin.Core.Utils;
 using Dythervin.Updaters.Main;
-using ICSharpCode.NRefactory.Visitors;
 using UnityEngine;
 using Object = UnityEngine.Object;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
 
 namespace Dythervin.ObjectPool.Component
 {
@@ -18,17 +17,17 @@ namespace Dythervin.ObjectPool.Component
     public class ComponentPool<T> : ObjectPoolBase<T>, IComponentPool<T>, IPlayModeListener, ISerializationCallbackReceiver
         where T : UnityEngine.Component
     {
-        private readonly Dictionary<ICallbacks, T> _objectsMap = new Dictionary<ICallbacks, T>();
-
         private static readonly Action<ICallbacks> DestroyedFunc = callbacks =>
         {
             if (!ApplicationExt.IsQuitting)
-            {
                 throw new Exception($"Pooled object can be destroyed only by pool ({((Object)callbacks).name})");
-            }
         };
 
+        private readonly Dictionary<ICallbacks, T> _objectsMap = new Dictionary<ICallbacks, T>();
+
         private readonly Action<ICallbacks> _returnToPoolFunc;
+
+        private Vector3 _defaultScale;
 
 #if ODIN_INSPECTOR
         [HideInEditorMode]
@@ -46,12 +45,21 @@ namespace Dythervin.ObjectPool.Component
 #endif
         private int _pooled;
 
-        private Vector3 _defaultScale;
+        public ComponentPool(T prefab, Action<T> onGet = null, Action<T> onRelease = null, Action<T> actionOnDestroy = null, bool onlyPoolCanDestroyObj = true,
+            bool collectionCheckDefault = DefaultCollectionCheck, int defaultCapacity = DefaultCapacity, int maxSize = DefaultMaxSize)
+            : base(collectionCheckDefault, onGet, onRelease, actionOnDestroy, defaultCapacity, maxSize)
+        {
+            OnlyPoolCanDestroyObj = onlyPoolCanDestroyObj;
+            Prefab = prefab;
+            OnDestroy += Destroy;
+            _returnToPoolFunc = ReturnToPool;
+            this.PlayModeSubscribe();
+        }
 
-        [field: SerializeField] public T Prefab { get; private set; }
-        [field: SerializeField] public Transform Parent { get; private set; }
-        [field: SerializeField] public bool OnlyPoolCanDestroyObj { get; private set; }
-        public int PrefabId { get; private set; }
+        protected ComponentPool()
+        {
+            _returnToPoolFunc = ReturnToPool;
+        }
 
 
         public T Get(Transform parent, bool setActive = true)
@@ -129,32 +137,24 @@ namespace Dythervin.ObjectPool.Component
             return obj;
         }
 
-        public ComponentPool(T prefab, Action<T> onGet = null, Action<T> onRelease = null, Action<T> actionOnDestroy = null, bool onlyPoolCanDestroyObj = true,
-            bool collectionCheckDefault = DefaultCollectionCheck, int defaultCapacity = DefaultCapacity, int maxSize = DefaultMaxSize)
-            : base(collectionCheckDefault, onGet, onRelease, actionOnDestroy, defaultCapacity, maxSize)
+        bool IPlayModeListener.MainThreadOnly => true;
+
+        void IPlayModeListener.OnEnterPlayMode()
         {
-            OnlyPoolCanDestroyObj = onlyPoolCanDestroyObj;
-            Prefab = prefab;
-            OnDestroy += Destroy;
-            _returnToPoolFunc = ReturnToPool;
+            OnEnterPlayMode();
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize() { }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
             this.PlayModeSubscribe();
         }
 
-        protected ComponentPool()
-        {
-            _returnToPoolFunc = ReturnToPool;
-        }
-
-        ~ComponentPool()
-        {
-            if (!ApplicationExt.IsPlaying || ApplicationExt.IsQuitting)
-                return;
-
-            if (ThreadExt.IsMain)
-                Clear();
-            else
-                Updater.Instance.OnUpdateOnce += Clear;
-        }
+        [field: SerializeField] public T Prefab { get; private set; }
+        [field: SerializeField] public Transform Parent { get; private set; }
+        [field: SerializeField] public bool OnlyPoolCanDestroyObj { get; private set; }
+        public int PrefabId { get; private set; }
 
         public void ReturnToPool(ICallbacks callbacks)
         {
@@ -172,6 +172,15 @@ namespace Dythervin.ObjectPool.Component
             Stack.Push(obj);
             _instanced--;
             _pooled++;
+        }
+
+        public void AddToPool(T obj)
+        {
+            ICallbacks pooledObject = obj.GetCallbacks();
+            pooledObject.OnDisabled += _returnToPoolFunc;
+            if (OnlyPoolCanDestroyObj)
+                pooledObject.OnDestroyed += DestroyedFunc;
+            _objectsMap.Add(pooledObject, obj);
         }
 
         protected T CreateObj(bool setActive)
@@ -197,21 +206,19 @@ namespace Dythervin.ObjectPool.Component
             return obj;
         }
 
-        public void AddToPool(T obj)
-        {
-            ICallbacks pooledObject = obj.GetCallbacks();
-            pooledObject.OnDisabled += _returnToPoolFunc;
-            if (OnlyPoolCanDestroyObj)
-                pooledObject.OnDestroyed += DestroyedFunc;
-            _objectsMap.Add(pooledObject, obj);
-        }
-
         protected override void OnReleased(T element)
         {
             base.OnReleased(element);
             GameObject gameObj = element.gameObject;
             if (gameObj.activeSelf)
                 gameObj.SetActive(false);
+        }
+
+        protected virtual void OnEnterPlayMode()
+        {
+            PrefabId = Prefab.GetInstanceID();
+            _defaultScale = Prefab.transform.localScale;
+            SetParent();
         }
 
         private void Destroy(T obj)
@@ -224,39 +231,28 @@ namespace Dythervin.ObjectPool.Component
             Object.Destroy(obj);
         }
 
-        bool IPlayModeListener.MainThreadOnly => true;
-
-        void IPlayModeListener.OnEnterPlayMode()
-        {
-            OnEnterPlayMode();
-        }
-
-        void ISerializationCallbackReceiver.OnBeforeSerialize() { }
-
-        void ISerializationCallbackReceiver.OnAfterDeserialize()
-        {
-            this.PlayModeSubscribe();
-        }
-
-        protected virtual void OnEnterPlayMode()
-        {
-            PrefabId = Prefab.GetInstanceID();
-            _defaultScale = Prefab.transform.localScale;
-
-            SetParent();
-        }
-
         private void SetParent()
         {
             if (Parent)
                 return;
-            
+
             Parent = new GameObject($"{typeof(T).Name} pool")
             {
                 isStatic = true,
                 transform = { parent = PersistentRoot.Get("ObjectPools").transform },
                 hideFlags = HideFlags.DontSaveInBuild | HideFlags.DontSaveInEditor
             }.transform;
+        }
+
+        ~ComponentPool()
+        {
+            if (!ApplicationExt.IsPlaying || ApplicationExt.IsQuitting)
+                return;
+
+            if (ThreadExt.IsMain)
+                Clear();
+            else
+                Updater.Instance.OnUpdateOnce += Clear;
         }
     }
 }
